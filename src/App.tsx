6b4c3be61466b7dd0a99c4835ad2c55333be09b2
Om Srivastava <79087911+omsrivastava512@users.noreply.github.com>
2024-09-15 18:26:30 +0530

    added an error toast because alert is ugly, optimised the row selector function

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import { Column } from 'primereact/column';
 import { OverlayPanel } from 'primereact/overlaypanel';
 import { Button } from 'primereact/button';
 import { InputText } from 'primereact/inputtext';
+import { Toast } from 'primereact/toast';
+        
         
 
 /** Interfaces Start */
@@ -39,27 +41,29 @@ import { InputText } from 'primereact/inputtext';
 
 
 export default function App() {
-    const [titles, setTitles] = useState<Title[]>([]);        
-    /** Selected rows are remembered because selectedTitles is gloabl to DataTable */
-    const [selectedTitles, setSelectedTitles] = useState<Title[] | null>(null);
-    /** For pagination and table render */
-    const [pagination, setPagination] = useState<Pagination | null>(null);
-    const [loading, setLoading] = useState<boolean>(true);
-    const [error, setError] = useState<string | null>(null);
-    
-    const op = useRef<OverlayPanel>(null)
-    const debounceTimeout = useRef<number | undefined>(undefined); 
+    /** Declarations start */
+        const [titles, setTitles] = useState<Title[]>([]);        
+        /** Selected rows are remembered because selectedTitles is gloabl to DataTable */
+        const [selectedTitles, setSelectedTitles] = useState<Title[] | null>(null);
+        /** For pagination and table render */
+        const [pagination, setPagination] = useState<Pagination | null>(null);
+        const [loading, setLoading] = useState<boolean>(true);
+        const [error, setError] = useState<string | null>(null);
+        
+        const op = useRef<OverlayPanel>(null)
+        const toast = useRef<Toast>(null);
+        const debounceTimeout = useRef<number | undefined>(undefined); 
 
-    /** Storing the desired field name to later use in maping **/
-    const fields : string[] = ["title", "place_of_origin", "artist_display", "inscriptions", "date_start", "date_end"];
-    const headers : string[] = fields.map(field => field.split('_').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')) // capitalising the fields
-
+        /** Storing the desired field name to later use in maping **/
+        const fields : string[] = ["title", "place_of_origin", "artist_display", "inscriptions", "date_start", "date_end"];
+        const headers : string[] = fields.map(field => field.split('_').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')) // capitalising the fields
+    /** Declaration ends */
     
     useEffect(()=>{
         updateTableStates(1);
         },[])
 
-    /** Just a regular fetch function **/
+    /** Fetch records based on page number and record limit **/
     const fetchData = async (page: number, limit:number): Promise<{ titles: Title[], pagination: Pagination}> => {
         console.log("Limit: "+limit);
         try {
@@ -95,7 +99,8 @@ export default function App() {
         }
     };
 
-    /** Updating states used in DataTable  **/
+
+    /** Updating states used in DataTable like titles and pagination  **/
     const updateTableStates = async(page:number):Promise<void> => {
         setLoading(true);
         const {titles, pagination} = await fetchData(page,12);
@@ -104,19 +109,19 @@ export default function App() {
         setPagination(pagination);
     }
     
-    /** Custom Selector Handler */
+    /** Toasts */
+    const showError = (message:string = "Undefined Error") => {
+        console.log(toast);
+        toast.current?.show({severity:'error', summary: 'Error', detail:message, life: 3000});
+    }
+
+    /** Custom Row Selector Handler */
     const rowSelectorHandler = async (e: ChangeEvent<HTMLInputElement>):Promise<void> => {
         const newValue:number = parseInt(e.target?.value)|| 0;
 
-        /** API Limitation */
-        if(newValue>100){
-            alert("Can not select more than 100 records at once");
-            return;
-        }
-
         /** Negative numbers */
         if(newValue<0){
-            alert("Please give a number between 0 to 100");
+            showError("Input can not be negative");
             return;
         } 
 
@@ -125,11 +130,15 @@ export default function App() {
           clearTimeout(debounceTimeout.current);
         }
         debounceTimeout.current = window.setTimeout(async():Promise<void> => {
-          if(newValue!=0){
-              const {titles} = await fetchData(1,newValue);
-              console.log(titles)
-              setSelectedTitles(titles);
-          }
+            if(newValue>100){
+                showError("Can not select more than 100 records at once");
+                return;
+            }       
+            if(newValue!=0){
+                const {titles} = await fetchData(1,newValue);
+                console.log(titles)
+                setSelectedTitles(titles);
+            }
           else setSelectedTitles(null);
         },500); 
     }
@@ -138,7 +147,7 @@ export default function App() {
     <div id='custom-selector' >
         <Button size= 'small' type="button" icon="pi pi-angle-down"onClick={(e) => op.current?.toggle(e)} />
         <OverlayPanel ref={op}>
-            <InputText type="number" min="0" className="p-inputtext-sm"  placeholder="Enter number of rows"
+            <InputText type="number" min="0" autoFocus className="p-inputtext-sm"  placeholder="Enter number of rows"
             onChange={rowSelectorHandler} />
         </OverlayPanel>
     </div>    
@@ -148,6 +157,7 @@ export default function App() {
 
     return (
         <div className="card">
+            <Toast ref={toast} />
             <DataTable 
                 value={titles} 
                 paginator 
